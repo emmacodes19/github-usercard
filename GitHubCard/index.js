9a68f8786139bc95972f4fbd2d9eb9104acce1d3
emmacodes19 <berdae@gmail.com>
2019-10-03 16:17:48 -0500

    first commit

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,7 +1,26 @@
 /* Step 1: using axios, send a GET request to the following URL 
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
+    
 */
+
+axios
+.get('https://api.github.com/users/emmacodes19')
+.then(response => {
+  console.log(response);
+  const card = document.querySelector('.cards');
+  let newCards = gitCard(response.data);
+  card.appendChild(newCards);
+  
+
+  
+})
+
+.catch((error) => {
+  console.log('some text', error);
+});
+  
+
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -45,6 +64,63 @@ const followersArray = [];
 </div>
 
 */
+
+
+
+
+function gitCard(oneObject) {
+  const 
+  mainCard = document.createElement('div'),
+  imgUrl = document.createElement('img'),
+  infoCard = document.createElement('div'),
+  names = document.createElement('h3'),
+  userNames = document.createElement('p'),
+  userLocation = document.createElement('p'),
+  userProfile = document.createElement('p'),
+  userProfileUrl = document.createElement('a'),
+  userFollowers = document.createElement('p'),
+  userFollowing = document.createElement('p'),
+  userBio = document.createElement('p')
+
+  mainCard.appendChild(imgUrl)
+  mainCard.appendChild(infoCard)
+  infoCard.appendChild(names)
+  infoCard.appendChild(userNames)
+  infoCard.appendChild(userLocation)
+  infoCard.appendChild(userProfile)
+  infoCard.appendChild(userProfileUrl)
+  infoCard.appendChild(userFollowers)
+  infoCard.appendChild(userFollowing)
+  infoCard.appendChild(userBio)
+  userProfile.appendChild(userProfileUrl)
+
+  mainCard.classList.add('card')
+  infoCard.classList.add('card-info')
+  names.classList.add('name')
+  userNames.classList.add('username')
+  
+
+
+  imgUrl.src = oneObject.avatar_url;
+  names.textContent =`Name${oneObject.name}`;
+  userNames.textContent = `Login ${oneObject.login}`;
+  userLocation.textContent= `Location ${oneObject.location}`;
+  userProfile.textContent = `Profile ${oneObject.name}`;
+  userFollowers.textContent = `Followers ${oneObject.followers}`;
+  userFollowing.textContent = `Following ${oneObject.following}`;
+  userBio.textContent =oneObject.bio;
+
+  
+
+  return mainCard;
+
+
+}
+
+
+
+
+
 
 /* List of LS Instructors Github username's: 
   tetondan
